40a3e7812d996215484c618fba0cc6c3fb464848
Agustin <gallegoagustin7@gmail.com>
2021-01-21 12:27:38 -0300

    Homework JSII done. I think some of the tests were wrong. Added definiciones.txt

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -204,7 +204,8 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 6 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
-
+  var tabla = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60]
+  return tabla
 }
 
 function tieneTresDigitos(numero){
@@ -222,7 +223,10 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-
+  var i = 8
+  do {
+    return (numero + 5 * i)
+  } while (i = 8)
 }
 
 
